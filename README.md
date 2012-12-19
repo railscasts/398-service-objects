413188e5f88cf2fbf096561ca89b8ce8993fec97
Ryan Bates <ryan@railscasts.com>
2012-12-18 21:45:09 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# RailsCasts Episode #398: Service Objects
+
+http://railscasts.com/episodes/398-service-objects
+
+Requires Ruby 1.9.2 or higher.
+
+
+### Commands used in this episode
+
+```
+rake spec
+rspec spec/features
+```
---
+++ auth-concerns/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :username, :password, :password_confirmation
+
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username
+  validates_confirmation_of :password
+  
+  include Authentication
+  include Searchable
+  include CsvConversion
+  include Inviter
+  include PasswordResettable
+end
